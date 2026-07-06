58cb15c7abaaa709c97d339f5ad9ca2681c078d5
SewminiWimalasooriya <sewminiwimalasooriya@gmail.com>
2026-07-06 13:35:36 +0530

    create slot, delete ,edit by ownerbackend features

--- backend/controllers/slotController.js
+++ backend/controllers/slotController.js
@@ -10,7 +10,7 @@ export const createSlot = async (req, res) => {
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        //logged owner's station
+        //logged owner's station. get the station id from the logged in user (owner)
         const stationId = req.user.apartment;
 
         const station = await Apartment.findById(stationId);
@@ -54,4 +54,138 @@ export const createSlot = async (req, res) => {
             message: error.message,
         });
     }
+}
+
+//get all slots
+export const getOwnerSlots = async (req, res) => {
+    try {
+        const stationId = req.user.apartment;
+
+        const slots = await Slot.find({
+            station: stationId,
+        }).sort({
+            date: 1,
+            startTime: 1,
+        });
+
+        if (!slots || slots.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No slots found for this station yet"
+            })
+        } else {
+            return res.status(200).json({
+                success: true,
+                count: slots.length,
+                slots,
+            });
+        }
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+//update slot
+export const updateSlot = async (req, res) => {
+    try {
+        const stationId = req.user.apartment;
+    
+
+        const slot = await Slot.findById(req.params.id);
+
+        
+        if (!slot) {
+            return res.status(404).json({
+                success: false,
+                message: "Slot not found",
+            });
+        }
+
+        if (slot.station.toString() !== stationId.toString()) {
+            return res.status(403).json({
+                success: false,
+                message: "Unauthorized",
+            });
+        }
+
+        const {
+            slotName,
+            date,
+            startTime,
+            endTime,
+            status,
+            isBooked,
+        } = req.body;
+
+        slot.slotName = slotName || slot.slotName;
+        slot.date = date || slot.date;
+        slot.startTime = startTime || slot.startTime;
+        slot.endTime = endTime || slot.endTime;
+        slot.status = status || slot.status;
+
+        if (typeof isBooked === "boolean") {
+            slot.isBooked = isBooked;
+        }
+
+        await slot.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Slot updated successfully",
+            slot,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+//slot delete
+export const deleteSlot = async (req,res) =>{
+    try{
+        const stationId = req.user.apartment;
+        const slot = await Slot.findById(req.params.id);
+
+        if (!slot) {
+      return res.status(404).json({
+        success: false,
+        message: "Slot not found",
+      });
+    }
+
+    if (slot.station.toString() !== stationId.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+     if (slot.isBooked) {
+      return res.status(400).json({
+        success: false,
+        message: "Booked slot cannot be deleted",
+      });
+    }
+
+    await Slot.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Slot deleted successfully",
+    });
+
+
+    }catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
--- backend/routes/slotRoutes.js
+++ backend/routes/slotRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import {createSlot} from "../controllers/slotController.js";
+import {createSlot, getOwnerSlots, updateSlot, deleteSlot} from "../controllers/slotController.js";
 import {protect} from "../middleware/auth.js";
+import { ownerOnly } from "../middleware/roleCheckMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create-slot", protect, createSlot);
+router.post("/create-slot", protect, ownerOnly, createSlot);
+router.get("/get-owner-slots",protect,getOwnerSlots);
+router.put("/update-owner-slot/:id",protect,ownerOnly,updateSlot);
+router.delete("/delete-slot/:id",protect,ownerOnly, deleteSlot);
 
 export default router;
